0d6157400d683546cbb7fb495579a064e8d1a6d7
lance <li6151770@126.com>
2023-12-03 12:46:57 +0800

    (feature)add doc base config

---
+++ src/main/java/com/lance/doc/DocHowtoApplication.java
@@ -0,0 +1,13 @@
+package com.lance.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocHowtoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DocHowtoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lance/doc/DocHowtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lance.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocHowtoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
